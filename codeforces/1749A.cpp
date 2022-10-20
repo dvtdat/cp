b34e4c430d860d27dc0806fae4e1849c8c608637
dvtdat <tiendat0417@gmail.com>
2022-10-20 21:30:09 +0700

    name change

---
+++ codeforces/1749A.cpp
@@ -0,0 +1,24 @@
+// tram kam
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int test; cin >> test;
+    while (test--) solve();
+}
+
+
+
