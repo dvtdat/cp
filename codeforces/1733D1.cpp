a4466293d539cae06fabe0996fdc0217cccf4f89
dvtdat <tiendat0417@gmail.com>
2023-01-23 22:27:59 +0700

    mung 2 no fun

--- codeforces/1733D1.cpp
+++ codeforces/1733D1.cpp
@@ -1,4 +1,4 @@
-// 
+// toi thuc su. khong xung' dang' voi' em...
 
 #include <bits/stdc++.h>
 #pragma GCC optimize("Ofast")
@@ -22,22 +22,44 @@ int a[maxN], b[maxN];
 
 void solve()
 {
-    int n, x, y; cin >> n >> x >> y;
+    ll n, x, y; cin >> n >> x >> y;
 
     memset(a, 0, sizeof a);
     memset(b, 0, sizeof b);
 
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    for (int i = 0; i < n; ++i) cin >> b[i];
+    for (int i = 0; i < n; ++i) 
+    {
+        char c; cin >> c; a[i] = c - '0';
+    }
+    for (int i = 0; i < n; ++i) 
+    {
+        char c; cin >> c; b[i] = c - '0';
+    }
 
     for (int i = 0; i < n; ++i) a[i] ^= b[i];
 
+    int cnt = 0;
+
     for (int i = 0; i < n; ++i)
     {
-        cout << a[i];
+        if (a[i]) cnt++;
     }
 
-    cout << '\n';
+    if (cnt & 1)
+    {
+        cout << -1 << '\n'; return;
+    }
+
+    if (cnt == 2)
+    {
+        for (int i = 0; i < n - 1; ++i) 
+            if (a[i] == 1 && a[i + 1] == 1)
+            {
+                cout << min(x, 2 * y) << '\n'; return;
+            }
+    }
+
+    cout << cnt / 2 * y << '\n';
 }
 
 int main()
@@ -60,4 +82,3 @@ int main()
 //     \__,_| \_/  \__\__,_|
 //
 //
-
