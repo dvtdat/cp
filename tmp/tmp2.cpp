c06526293ef010574e98a4686ff784151dff4bf8
dvtdat <tiendat0417@gmail.com>
2023-09-16 21:37:37 +0700

    testing #2

--- tmp/tmp2.cpp
+++ tmp/tmp2.cpp
@@ -1,5 +1,54 @@
-#include <iostream>
+// 
 
-int main() {
-    std::cout << "hello world!!!!!!!!!!\n";
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef long long ll;
+typedef long double ld;
+
+const ll infLL = 2e18 + 7;
+const int inf = 2e9 + 7;
+const int maxN = 200010;
+const ll MOD = 998244353;
+const double eps = 1e-12;
+
+void solve()
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i) 
+    {
+        cout << i << '\n';
+    }
 }
+
+void setIO(string name)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    if (!name.size()) return;
+
+    #ifndef ONLINE_JUDGE
+        freopen((name + ".inp").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    #endif
+}
+
+int main()
+{
+    setIO("");
+    int test = 1; //cin >> test;
+    while (test--) solve();
+}
+
+//         _       _      _
+//        | |     | |    | |
+//      __| |_   _| |_ __| |
+//     / _` \ \ / / __/ _` |
+//    | (_| |\ V /| || (_| |
+//     \__,_| \_/  \__\__,_|
+//
+//
+
