0b3b017ba64c9464b821ad7774708b81504e99c6
dvtdat <tiendat0417@gmail.com>
2025-07-26 15:40:02 +0700

    2028b

---
+++ codeforces/2028B.cpp
@@ -0,0 +1,58 @@
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef pair<int, ii> iii;
+typedef long double ld;
+
+const ll infLL = 2e18 + 7;
+const int inf = 2e9 + 7;
+const int maxN = 200010;
+const ll MOD = 998244353;
+const double eps = 1e-12;
+
+// NOT WORKING
+
+void solve() {
+  ll n, b, c; cin >> n >> b >> c;
+
+  if (b == 0) {
+    if (c >= n - 2) cout << n << '\n';
+    else cout << -1 << '\n';
+    return;
+  }
+}
+
+void setIO(string name) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  if (!name.size())
+    return;
+
+#ifndef ONLINE_JUDGE
+  freopen((name + ".inp").c_str(), "r", stdin);
+  // freopen((name + ".out").c_str(), "w", stdout);
+#endif
+}
+
+int main() {
+  setIO("text");
+  int test;
+  cin >> test;
+  while (test--)
+    solve();
+}
+
+//         _       _      _
+//        | |     | |    | |
+//      __| |_   _| |_ __| |
+//     / _` \ \ / / __/ _` |
+//    | (_| |\ V /| || (_| |
+//     \__,_| \_/  \__\__,_|
+//
+//
